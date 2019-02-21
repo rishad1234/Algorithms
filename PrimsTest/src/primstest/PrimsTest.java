f9d7a43b2576166c915384aa74aae9ca9de9820f
Rishadul Islam <rishadulislamkhan@gmail.com>
2019-02-21 13:40:34 +0600

    exception is not handled

--- PrimsTest/src/primstest/PrimsTest.java
+++ PrimsTest/src/primstest/PrimsTest.java
@@ -10,14 +10,16 @@ public class PrimsTest {
 
    
     private static List<Vertex> vertices = new ArrayList<>();
-    private static PriorityQueue<Edge> edges = new PriorityQueue<>();
+    private static List<Edge> edges = new ArrayList<>();
+    private static PriorityQueue<Edge> edgeHeap = new PriorityQueue<>();
+    private static int cost = 0;
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the number of vertices: ");
         int numberOfVertices = input.nextInt();
         
         for(int i = 0; i < numberOfVertices; i++){
-            vertices.add(new Vertex(i + 1, false, Integer.MAX_VALUE));
+            vertices.add(new Vertex(i + 1, false, Integer.MAX_VALUE, 0));
         }
         
         System.out.println("Enter the number of edges: ");
@@ -28,19 +30,35 @@ public class PrimsTest {
             int endVertex = input.nextInt();
             int weight = input.nextInt();
             
-            edges.add(new Edge(vertices.get(startVertex - 1), vertices.get(endVertex - 1), weight));
+            vertices.get(startVertex - 1).addEdge(new Edge(vertices.get(startVertex - 1), vertices.get(endVertex - 1), weight));
         }
+        prims(vertices.get(0));
+        showMst();
+    }
+    
+    public static void prims(Vertex v){
+        vertices.remove(v);
         
-        //main algorithm
-        
-//        for(Vertex v : vertices){
-//        
-//        }
-
-        while(!edges.isEmpty()){
-            System.out.println(edges.poll().getWeight());
+        while(!vertices.isEmpty()){
+            for(Edge edge : v.getAdjacencies()){
+                if(vertices.contains(edge.getEndVertex())){
+                    edgeHeap.add(edge);
+                }
+            }
+            
+            Edge minEdge = edgeHeap.remove();
+            
+            edges.add(minEdge);
+            cost += minEdge.getWeight();
+            vertices.remove(minEdge.getEndVertex());
         }
-        
+    }
+    
+    public static void showMst(){
+        System.out.println("cost of mst: " + cost);
+        for(Edge e : edges){
+            System.out.println(e.getStartVertex() + " " + e.getEndVertex() + " " + e.getWeight());
+        }
     }
     
 }
--- PrimsTest/src/primstest/Vertex.java
+++ PrimsTest/src/primstest/Vertex.java
@@ -1,15 +1,39 @@
 
 package primstest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Vertex {
     private int vertexName;
     private boolean visited;
     private int cost;
+    private int parent;
+    private List<Edge> adjacencies;
 
-    public Vertex(int vertexName, boolean visited, int cost) {
+    public int getParent() {
+        return parent;
+    }
+
+    public void setParent(int parent) {
+        this.parent = parent;
+    }
+
+    public List<Edge> getAdjacencies() {
+        return adjacencies;
+    }
+
+    public void setAdjacencies(List<Edge> adjacencies) {
+        this.adjacencies = adjacencies;
+    }
+
+    public Vertex(int vertexName, boolean visited, int cost, int parent) {
         this.vertexName = vertexName;
         this.visited = visited;
         this.cost = cost;
+        this.parent = parent;
+        this.adjacencies = new ArrayList<>();
+        this.visited = false;
     }
 
     public int getVertexName() {
@@ -35,5 +59,15 @@ public class Vertex {
     public void setCost(int cost) {
         this.cost = cost;
     }
+    
+    public void addEdge(Edge e){
+        this.adjacencies.add(e);
+    }
+
+    @Override
+    public String toString() {
+        return "" + vertexName + " ";
+    }
+    
     
 }
