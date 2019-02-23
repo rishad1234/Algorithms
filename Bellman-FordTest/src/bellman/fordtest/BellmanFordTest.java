a6c04e77ef4cbfadb555964884f270a73a863ef7
Rishadul Islam <rishadulislamkhan@gmail.com>
2019-02-23 10:10:57 +0600

    bellman ford algorithm implemented

---
+++ Bellman-FordTest/src/bellman/fordtest/BellmanFordTest.java
@@ -0,0 +1,103 @@
+
+package bellman.fordtest;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class BellmanFordTest {
+
+    private static List<Vertex> vertices = new ArrayList<>();
+    private static List<Edge> edges = new ArrayList<>();
+    private static List<Vertex> shortestPath = new ArrayList<>();
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter the number of vertices: ");
+        int numberOfVertices = input.nextInt();
+        
+        for(int i = 0; i < numberOfVertices; i++){
+            vertices.add(new Vertex(i + 1));
+        }
+        
+        System.out.println("Enter the number of edges: ");
+        int numberOfEdges = input.nextInt();
+        
+        for(int i = 0; i < numberOfEdges; i++){
+            int start = input.nextInt();
+            int end = input.nextInt();
+            int weight  = input.nextInt();
+            
+            edges.add(new Edge(vertices.get(start - 1), vertices.get(end - 1), weight));
+        }
+        
+        System.out.println("Enter the source Vertex: ");
+        int source =input.nextInt();
+        
+        bellmanFord(vertices.get(source - 1));
+        
+        System.out.println("Enter the target Vertex: ");
+        int target = input.nextInt();
+        
+        shortestPathTo(vertices.get(target - 1));
+        
+        System.out.println("shortest path: ");
+        for(Vertex v : shortestPath){
+            System.out.println(v.getVertexName() + " " + v.getDistance());
+        }
+        
+        
+        System.out.println("All results: ");
+        for(Vertex v : vertices){
+            System.out.println(v.getVertexName() + " " + v.getDistance() + " " + v.getPredecessor());
+        }
+    }
+    
+    public static void bellmanFord(Vertex source){
+        source.setDistance(0);
+        
+        for(int i = 0; i < vertices.size() - 1; i++){
+            
+            for(Edge e : edges){
+                Vertex u = e.getStartVertex();
+                Vertex v = e.getEndVertex();
+                
+                if(u.getDistance() == Integer.MAX_VALUE){
+                    continue;
+                }
+                
+                int distance = u.getDistance() + e.getWeight();
+                
+                if(distance < v.getDistance()){
+                    v.setDistance(distance);
+                    v.setPredecessor(u);
+                }
+            }
+        }
+        
+        for(Edge e : edges){
+            if(e.getStartVertex().getDistance() != Integer.MAX_VALUE){
+                
+                if(hasCycle(e)){
+                    System.out.println("There is a negative cycle in this graph");
+                    return;
+                }
+            }
+        }
+    }
+
+    private static boolean hasCycle(Edge e) {
+        return e.getStartVertex().getDistance() + e.getWeight() < e.getEndVertex().getDistance();
+    }
+    
+    public static void shortestPathTo(Vertex target){
+        for(Vertex v = target; v!= null; v = v.getPredecessor()){
+            shortestPath.add(v);
+        }
+
+        Collections.reverse(shortestPath);
+    }
+    
+    
+}
---
+++ Bellman-FordTest/src/bellman/fordtest/Vertex.java
@@ -0,0 +1,72 @@
+
+package bellman.fordtest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vertex implements Comparable<Vertex>{
+    private int vertexName;
+    private List<Edge> adjacencies;
+    private boolean visied;
+    private int distance = Integer.MAX_VALUE;
+    private Vertex predecessor;
+
+    public Vertex(int vertexName) {
+        this.adjacencies = new ArrayList<>();
+        this.vertexName = vertexName;
+    }
+
+    public int getVertexName() {
+        return vertexName;
+    }
+
+    public void setVertexName(int vertexName) {
+        this.vertexName = vertexName;
+    }
+
+    public List<Edge> getAdjacencies() {
+        return adjacencies;
+    }
+
+    public void setAdjacencies(List<Edge> adjacencies) {
+        this.adjacencies = adjacencies;
+    }
+
+    public boolean isVisied() {
+        return visied;
+    }
+
+    public void setVisied(boolean visied) {
+        this.visied = visied;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+    
+    public void addAdjacent(Edge e){
+        this.adjacencies.add(e);
+    }
+
+    public Vertex getPredecessor() {
+        return predecessor;
+    }
+
+    public void setPredecessor(Vertex predecessor) {
+        this.predecessor = predecessor;
+    }
+
+    @Override
+    public String toString() {
+        return "" + vertexName + " ";
+    }
+
+    @Override
+    public int compareTo(Vertex o) {
+        return this.getDistance() - o.getDistance();
+    } 
+}
