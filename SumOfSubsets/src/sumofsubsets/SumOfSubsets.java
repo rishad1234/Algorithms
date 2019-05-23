7caf25cedb874a6f9726bba061744f6874bc8ad1
rishad <rishadulislamkhan@gmail.com>
2019-05-23 16:51:15 +0600

    it is not showing the multiple ouytputs

--- SumOfSubsets/src/sumofsubsets/SumOfSubsets.java
+++ SumOfSubsets/src/sumofsubsets/SumOfSubsets.java
@@ -27,30 +27,37 @@ public class SumOfSubsets {
         System.out.println("Maximum: ");
         int maximum = input.nextInt();
         Arrays.sort(sets);
-        sumOfSub(0, 0, maximum - sets[0], maximum);     
+        for(int i : sets){
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+        sumOfSub(0, 0, maximum, maximum);
     }
     
     public static void sumOfSub(int s, int k, int r, int maximum){
-        x[k] = 1;
-        if(s + sets[k] == maximum){
-            System.out.println("dhukse3");
-            printSolution(k);
-        }else if(s + sets[k] + sets[k + 1] <= maximum){
-            System.out.println("dhukse");
-            sumOfSub(s + sets[k], k + 1, r - sets[k], maximum);
-        }
-        if((s + r - sets[k] >= maximum) && (s + sets[k + 1] <= maximum)){
-            x[k] = 0;
-            System.out.println("dhukse 2");
-            sumOfSub(s, k + 1, r - sets[k], maximum);
+        if(k < x.length - 1){
+            x[k] = 1;
+            if(s + sets[k] == maximum){
+                printSolution(k);
+            }else if(s + sets[k] + sets[k + 1] <= maximum){
+                sumOfSub(s + sets[k], k + 1, r - sets[k], maximum);
+            }
+            if((s + r - sets[k] >= maximum) || (s + sets[k + 1] <= maximum)){
+                x[k] = 0;
+                sumOfSub(s, k + 1, r - sets[k], maximum);
+            }   
         }
         
     }
     
     public static void printSolution(int k){
-        for(int i  = 0; i < k; i++){
+        for(int i  = 0; i < x.length; i++){
             System.out.print(x[i]);
+            if(k == i){
+                x[i] = 0;
+            }
         }
         System.out.println("");
+        
     }
 }
