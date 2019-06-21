71da185f1b6ce6c714692af7aba592f8dc3c6f60
Rishadul Islam <rishadulislamkhan@gmail.com>
2019-06-21 10:22:40 +0600

    kadanes algorithm modified

--- kadanesAlgorithm/src/kadanesalgorithm/KadanesAlgorithm.java
+++ kadanesAlgorithm/src/kadanesalgorithm/KadanesAlgorithm.java
@@ -22,20 +22,40 @@ public class KadanesAlgorithm {
             array[i] = input.nextInt();
         }
         
-        System.out.println(kadane());
+        int[] ar = kadane();
+        for(int i = 0; i < ar.length; i++){
+            System.out.println(ar[i]);
+        }
+      
     }
     
-    public static int kadane(){
-        int currentMax = array[0];
-        int globalMax = array[0];
+    public static int[] kadane(){
+        int currentMax = 0;
+        int globalMax = 0;
         
-        for(int i = 1; i < array.length; i++){
-            currentMax = Math.max(array[i], currentMax + array[i]);
+        int maxStart = -1;
+        int maxEnd = -1;
+        
+        int currentStart = 0;
+        
+        for(int i = 0; i < array.length; i++){
+            
+            currentMax += array[i];
+            
+            if(currentMax < 0){
+                currentMax = 0;
+                currentStart = i + 1;
+            }
             
             if(currentMax > globalMax){
+                
                 globalMax = currentMax;
+                maxStart = currentStart;
+                maxEnd = i;
             }
         }
-        return globalMax;
+        
+        int[] ar = {globalMax, maxStart, maxEnd};
+        return ar;
     }  
 }
