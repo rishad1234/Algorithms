4bbabfcfa75ecc51b263ce7ebc5479ab2edab680
Rishadul Islam <rishadulislamkhan@gmail.com>
2020-05-04 14:05:08 +0600

    updated

---
+++ Percolation/src/percolation/Percolation.java
@@ -0,0 +1,112 @@
+package percolation;
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+/**
+ *
+ * @author Admin
+ */
+public class Percolation {
+
+    private int n;
+    private int top;
+    private int bottom;
+    private WeightedQuickUnionUF unionFind;
+    private byte[] site;
+
+    public static void main(String[] args) {
+        PercolationStats stats = new PercolationStats(200, 100);
+        System.out.println("mean                    = " + stats.mean());
+        System.out.println("stddev                  = " + stats.stddev());
+        System.out.println("95% confidence interval = [" + stats.confidenceLo()
+                + ", " + stats.confidenceHi() + "]");
+    }
+
+    public Percolation(int N) {
+        n = N;
+        unionFind = new WeightedQuickUnionUF(n * n + 2);
+        top = n * n;
+        bottom = n * n + 1;
+        site = new byte[n * n];
+    }
+
+    public void open(int i, int j) {
+        checkError(i, j);
+        if (isOpen(i, j)) {
+            return;
+        }
+        int currentSite = convert2dTo1dCoord(i, j);
+        this.site[currentSite] = 1;
+
+        if (i == 1 && !unionFind.connected(currentSite, j)) {
+            unionFind.union(currentSite, top);
+        }
+
+        if (i == n) {
+            unionFind.union(currentSite, bottom);
+        }
+
+        if (i < n) {
+            if (isOpen(i + 1, j)) {
+                unionFind.union(currentSite, convert2dTo1dCoord(i + 1, j));
+            }
+        }
+        if (i > 1) {
+            if (isOpen(i - 1, j)) {
+                unionFind.union(currentSite, convert2dTo1dCoord(i - 1, j));
+            }
+        }
+        if (j > 1) {
+            if (isOpen(i, j - 1)) {
+                unionFind.union(currentSite, convert2dTo1dCoord(i, j - 1));
+            }
+        }
+        if (j < n) {
+            if (isOpen(i, j + 1)) {
+                unionFind.union(currentSite, convert2dTo1dCoord(i, j + 1));
+            }
+        }
+    }
+
+    public boolean checkError(int i, int j) {
+        if (i < 1 || i > n || j < 1 || j > n) {
+            throw new IndexOutOfBoundsException();
+        }
+        return true;
+    }
+
+    public boolean isOpen(int i, int j) {
+        checkError(i, j);
+        if (site[convert2dTo1dCoord(i, j)] == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    private int convert2dTo1dCoord(int i, int j) {
+        int pos = n * (i - 1) + j - 1;
+        return pos;
+    }
+
+    public boolean isFull(int i, int j) {
+        checkError(i, j);
+        if (!isOpen(i, j)) {
+            return false;
+        }
+        int currentSite = convert2dTo1dCoord(i, j);
+        if (unionFind.connected(top, currentSite)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean percolates() {
+        if (unionFind.connected(top, bottom)) {
+            return true;
+        }
+        return false;
+    }
+
+}
