705bd45fa2a18f34d978b09e0dd634b57ed7f75d
Rishadul Islam <rishadulislamkhan@gmail.com>
2020-09-02 21:50:12 +0600

    updated

---
+++ FCFSDisk/src/fcfsdisk/FCFSDisk.java
@@ -0,0 +1,53 @@
+
+package fcfsdisk;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FCFSDisk {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int movement = 0;
+        
+        System.out.println("Number of heads: ");
+        int numOfHeads = input.nextInt();
+        
+        System.out.println("number of cylinder request: ");
+        int cylinderRequest = input.nextInt();
+        
+        System.out.println("Cylinder request: ");
+        int[] cylinders = new int[cylinderRequest];
+        for(int i = 0; i < cylinderRequest; i++){
+            int temp = input.nextInt();
+            if(temp > numOfHeads){
+                System.out.println("Enter again: ");
+                temp = input.nextInt();
+                cylinders[i] = input.nextInt();
+            }else{
+                cylinders[i] = temp;
+            }
+            
+        }
+        
+        System.out.println("Current head position: ");
+        int head = input.nextInt();
+        
+        System.out.print(head + " ");
+        for(int i = 0; i < cylinderRequest; i++){
+            System.out.print(cylinders[i] + " ");
+            if(i == 0){
+                movement += Math.abs(head - cylinders[i]);
+                //System.out.println("movement: " + movement);
+            }else{
+                movement += Math.abs(cylinders[i] - cylinders[i - 1]);
+                //System.out.println("movement: " + movement);
+            }
+        }
+        System.out.println("");
+        System.out.println(movement);
+        
+    }
+    
+}
---
+++ SSTFDisk/src/sstfdisk/SSTFDisk.java
@@ -0,0 +1,57 @@
+
+package sstfdisk;
+
+import java.util.Scanner;
+
+public class SSTFDisk {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int movement = 0;
+        
+        System.out.println("Number of heads: ");
+        int numOfHeads = input.nextInt();
+        
+        System.out.println("number of cylinder request: ");
+        int cylinderRequest = input.nextInt();
+        
+        System.out.println("Cylinder request: ");
+        int[] cylinders = new int[cylinderRequest];
+        int[] flag = new int[cylinderRequest];
+        
+        for(int i = 0; i < cylinderRequest; i++){
+            int temp = input.nextInt();
+            if(temp > numOfHeads){
+                System.out.println("Enter again: ");
+                temp = input.nextInt();
+                cylinders[i] = input.nextInt();
+            }else{
+                cylinders[i] = temp;
+            }
+        }
+        
+        System.out.println("Current head position: ");
+        int head = input.nextInt();
+        System.out.print(head + " ");
+        for(int i = 0; i < cylinderRequest; i++){
+            int index = 0;
+            int min = Integer.MAX_VALUE;
+            
+            for(int j = 0; j < cylinderRequest; j++){
+                int temp = Math.abs(head - cylinders[j]);
+                if(temp < min && flag[j] == 0){
+                    index = j;
+                    min = temp;
+                }
+            }
+            flag[index] = 1;
+            movement += min;
+            head = cylinders[index];
+            System.out.print(head + " ");
+        }
+        System.out.println("");
+        System.out.println(movement);
+
+    }
+    
+}
